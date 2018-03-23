5a8788d1e69d636dd90367b91c1a5c70eac96285
sami-mai <samirah.maison@gmail>
2018-03-23 16:49:16 +0300

    create and import profile service

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
+  providers:[ProfileService],
   styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent implements OnInit {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"https://api.github.com/users/sami-mai?access_token=da371b1508c09a2d38454e31b3a2468e861c58ce"
 };
