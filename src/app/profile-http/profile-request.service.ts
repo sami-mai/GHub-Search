6a0bbc7055d3d08e28d3971b1b07036ad6e226dc
sami-mai <samirah.maison@gmail>
2018-03-25 19:42:07 +0300

    add and define getProfileInfo, getProfileRepo & updateProfile(username) functions respectively and import userProfile and userRepos classes

--- src/app/profile-http/profile-request.service.ts
+++ src/app/profile-http/profile-request.service.ts
@@ -1,48 +1,91 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import {environment} from '../../environments/environment'
-import {UserProfile} from  '../user-class/user-profile'
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {UserProfile} from  '../user-class/user-profile';
+import {UserRepos} from '../user-class/user-repos';
 
 @Injectable()
 export class ProfileRequestService {
 
 private _apiKey:string = environment.gitHub_apiKey;
-login:string = 'sami-mai'
+private login:string;
 profile:UserProfile;
+repos:UserRepos;
 
 
 constructor(private http:HttpClient) {
-this.profile=new UserProfile("","","");
+this.profile=new UserProfile("","","","","",)
+this.login = 'sami-mai';
+this.repos=new UserRepos("","","");
   }
 
   getProfileInfo(){
 
     interface ApiResponse{
+
+        name:string;
         login:string;
         email:string;
-        avatar_url:string
+        avatar_url:string;
+        html_url:string;
 
       }
     let promise =new Promise((resolve,reject)=>{
     this.http.get<ApiResponse>("https://api.github.com/users/" + this.login +
     "?access_token=" + this._apiKey).toPromise().then(response=>{
 
+      this.profile.name=response.name
       this.profile.login=response.login
       this.profile.email=response.email
       this.profile.avatar_url=response.avatar_url
+      this.profile.html_url=response.html_url
 
       resolve()
       },
       error=>{
+      this.profile.name=''
       this.profile.login=''
       this.profile.email=''
       this.profile.avatar_url=''
+      this.profile.html_url=''
       reject(error)
         }
       )
    })
    return promise
   }
+
+getProfileRepo(){
+  interface ApiResponse{
+
+    name:string;
+    repo_url:string;
+    description:string
+
+    }
+  let promise =new Promise((resolve,reject)=>{
+  this.http.get<ApiResponse>("https://api.github.com/users/" + this.login +
+  "/repos?access_token=" + this._apiKey).toPromise().then(response=>{
+
+    this.repos.name=response.name
+    this.repos.repo_url=response.repo_url
+    this.repos.description=response.description
+
+    resolve()
+    },
+    error=>{
+
+    reject(error)
+      }
+    )
+ })
+ return promise
+}
+
+updateProfile(username:string){
+  this.login=username;
+}
+
 }
 
 
