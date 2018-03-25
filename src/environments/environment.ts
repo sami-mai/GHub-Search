23f5d890a70cf168a0c11554937004a894824d9a
sami-mai <samirah.maison@gmail>
2018-03-25 10:49:51 +0300

    update src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
-// The file contents for the current environment will overwrite these during build.
+gi// The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
   production: false,
-  apiUrl:"https://api.github.com/users/sami-mai?access_token=da371b1508c09a2d38454e31b3a2468e861c58ce"
+  gitHub_apiKey:'da371b1508c09a2d38454e31b3a2468e861c58ce',
+  gitHub_apiUrl:'https://api.github.com/users/sami-mai?access_token=da371b1508c09a2d38454e31b3a2468e861c58ce'
 };
