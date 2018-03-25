749e0e0407cfc517496210180a5df04dfe7bd2fd
sami-mai <samirah.maison@gmail>
2018-03-25 19:51:13 +0300

    add and define findProfile function and import userProfile and userRepos classes, httpclient module and profilerequestService

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileRequestService } from '../profile-http/profile-request.service';
+import {UserProfile} from '../user-class/user-profile';
+import {UserRepos} from '../user-class/user-repos';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  // providers:[ProfileRequestService],
+  providers:[ProfileRequestService],
   styleUrls: ['./search-results.component.css']
 })
+
 export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  profile:UserProfile;
+  repos:UserRepos;
+  username:string;
 
-  ngOnInit() {
+
+  constructor(private profileRequest:ProfileRequestService) {
+
   }
 
+  findProfile(){
+    this.profileRequest.updateProfile(this.username)
+    this.profile=this.profileRequest.profile
+    this.repos=this.profileRequest.repos
+  }
+
+  ngOnInit() {
+    this.profileRequest.getProfileInfo()
+    console.log(this.profile);
+
+    this.profileRequest.getProfileRepo()
+    console.log(this.repos);
+
+  }
 }
