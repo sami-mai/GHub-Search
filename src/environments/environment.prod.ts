2f75fd01499bc6bb3e4594212556ac04aae2c7ef
sami-mai <samirah.maison@gmail>
2018-03-26 12:44:29 +0300

    rectify error with form submission

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  gitHubapiKey:'da371b1508c09a2d38454e31b3a2468e861c58ce',
+  gitHub_apiUrl:'https://api.github.com/users/sami-mai?access_token=da371b1508c09a2d38454e31b3a2468e861c58ce'
 };
